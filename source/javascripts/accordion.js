25a54eda7154826051b0ae34f13f76dd7713b723
Joan Puigcerver <joapuiib@gmail.com>
2021-07-24 18:56:57 +0200

    accordion animation

--- source/javascripts/accordion.js
+++ source/javascripts/accordion.js
@@ -3,13 +3,13 @@ function onclickAccordion(evt) {
   $header.toggleClass("accordion-active");
 
   $content = $header.closest('div[class=accordion]').find(".accordion-content");
-  $content.toggleClass("accordion-content-active");
+  // $content.toggleClass("accordion-content-active");
 
-  // if($content.css("max-height") == "0px"){
-  //   $content.css("max-height", "100px")
-  // } else {
-  //   $content.css("max-height", "0px")
-  // }
+  if($content.css("max-height") == "0px"){
+    $content.css("max-height", $content[0].scrollHeight)
+  } else {
+    $content.css("max-height", "0px")
+  }
 }
 
 window.onclickAccordion = onclickAccordion;
